21db6106a8e36d5cf604dbf460a57e6d39834264
Lemuel Pereira <lemuelpjr@gmail.com>
2019-05-25 10:15:33 -0300

    add exercise 1 solution

--- script.js
+++ script.js
@@ -9,3 +9,17 @@ Mark and John are trying to compare their BMI (Body Mass Index), which is calcul
 3. Create a boolean variable containing information about whether Mark has a higher BMI than John.
 4. Print a string to the console containing the variable from step 3. (Something like "Is Mark's BMI higher than John's? true"). 
 */
+
+var bmiJohn, bmiMark, heightJohn, heightMark, massJohn, massMark, higherBmi;
+
+heightJohn = 180;
+massJohn = 70;
+heightMark = 170;
+massMark = 85;
+
+bmiJohn = heightJohn * heightJohn / massJohn;
+bmiMark = heightMark * heightMark / massMark;
+
+higherBmi = bmiMark > bmiJohn;
+
+console.log('Is Mark\'s BMI higher than John\'s? ' + higherBmi);
