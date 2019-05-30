b7f78e626f8ed20dd5d82bce9b80bcf26a33fb05
Lemuel Pereira <lemuelpjr@gmail.com>
2019-05-30 09:54:23 -0300

    add exercise 5.1 instrunctions

--- script.js
+++ script.js
@@ -113,6 +113,8 @@ Let's remember the first coding challenge where Mark and John compared their BMI
 Remember: BMI = mass / height^2 = mass / (height * height). (mass in kg and height in meter).
 */
 
+/* SOLUTION
+
 var john = {
   fullName: 'John Doe',
   mass: 78, // kg
@@ -140,3 +142,21 @@ if (john.calcBMI() > mark.calcBMI()) {
 } else {
   console.log(john.fullName + ' and ' + mark.fullName + ' have the same BMI.');
 }
+*/
+
+/*****************************
+* CODING CHALLENGE 5.1
+*/
+
+/*
+Remember the tip calculator challenge? Let's create a more advanced version using everything we learned!
+
+This time, John and his family went to 5 different restaurants. The bills were $124, $48, $268, $180 and $42.
+John likes to tip 20% of the bill when the bill is less than $50, 15% when the bill is between $50 and $200, and 10% if the bill is more than $200.
+
+Implement a tip calculator using objects and loops:
+1. Create an object with an array for the bill values
+2. Add a method to calculate the tip
+3. This method should include a loop to iterate over all the paid bills and do the tip calculations
+4. As an output, create 1) a new array containing all tips, and 2) an array containing final paid amounts (bill + tip). HINT: Start with two empty arrays [] as properties and then fill them up in the loop.
+*/
