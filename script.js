9a6509c74b7995d94a082c15e87d60c0a9dd72c9
Lemuel Pereira <lemuelpjr@gmail.com>
2019-05-31 08:01:25 -0300

    add exercise 5.2 instructions

--- script.js
+++ script.js
@@ -196,3 +196,17 @@ restaurantMeal.tipCalc();
 
 console.log(restaurantMeal.tips);
 console.log(restaurantMeal.finalCheks);
+
+/*****************************
+* CODING CHALLENGE 5.2
+*/
+
+/*
+Mark's family also went on a holiday, going to 4 different restaurants. The bills were $77, $375, $110, and $45.
+Mark likes to tip 20% of the bill when the bill is less than $100, 10% when the bill is between $100 and $300, and 25% if the bill is more than $300 (different than John).
+
+5. Implement the same functionality as before, this time using Mark's tipping rules
+6. Create a function (not a method) to calculate the average of a given array of tips. HINT: Loop over the array, and in each iteration store the current sum in a variable (starting from 0). After you have the sum of the array, divide it by the number of elements in it (that's how you calculate the average)
+7. Calculate the average tip for each family
+8. Log to the console which family paid the highest tips on average
+*/
